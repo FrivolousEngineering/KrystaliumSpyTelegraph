72b0903268cd5d1f2c970e30bf72cf947631cadd
Jaime van Kessel <nallath@gmail.com>
2025-01-03 15:02:56 +0100

    Add hasPaper code (not working yet)

---
+++ PrinterTest.py
@@ -0,0 +1,69 @@
+import fcntl
+import struct
+import os
+import time
+
+
+USBLP_GET_STATUS = 0x060b
+
+from escpos import printer as escposPrinter
+import subprocess
+
+
+def resetUsbPrinter():
+    subprocess.run(["sudo", "rmmod", "usblp"])
+    subprocess.run(["sudo", "modprobe", "usblp"])
+    subprocess.run(["sudo", "chmod", "666", "/dev/usb/lp1"])
+
+
+def hasPaper():
+    try:
+        # Story time! The escpos library doesn't handle the paper checking correctly. For some reason the default
+        # usb printing driver does. But that driver doesn't play well if i want to send images. So, as any "sane"
+        # developer, it makes me think "Well, why not create a monster of frankenstein".
+        # But the problem with monsters are is that they start shouting "WHY HAVE YOU CREATED ME FATHER"
+        # That is where the resetUsbPrinter comes in.
+        # Every time we send any command over USB to the printer via escpos, the linux kernel is very graceful with
+        # removing the /dev/usb/lp1. The only way to get it back is, you guessed it, restart the module
+        # May god have mercy on my soul, for these sins should not give me any.
+        resetUsbPrinter()
+        with open("/dev/usb/lp1", "r+b") as printer:
+            # Send paper status command
+            printer.write(b'\x10\x04\x04')
+            printer.flush()
+
+            # Get status using ioctl
+            status = struct.unpack('B', fcntl.ioctl(printer, USBLP_GET_STATUS, b'\x00'))[0]
+            result = f"{status:08b}"  # Convert status to binary
+            paper_present = result[2] == "1"  # Check paper bit
+
+            # Graceful close: Reset printer to avoid device lock
+           # printer.write(b'\x1b\x40')  # ESC @ (reset command)
+           # printer.flush()
+
+            return paper_present
+    except FileNotFoundError:
+        print("Printer not found at /dev/usb/lp1.")
+        return False
+    except Exception as e:
+        print("Error reading printer status:", e)
+        return False
+
+
+if hasPaper():
+    try:
+        p = escposPrinter.Usb(0x28e9, 0x0289, out_ep=0x03, profile="ZJ-5870")
+        p.control("LF")
+        p.close()
+    except Exception as e:
+        print("Error printing:", e)
+else:
+    print("NO PAPER :(")
+
+
+
+
+#reset_usb_printer()
+#status = p.device.read(p.in_ep)
+#print(status)
+
--- main.py
+++ main.py
@@ -5,8 +5,13 @@ import sys
 import random
 import threading
 from typing import Optional
-
+import os
 import requests
+import fcntl
+import struct
+import subprocess
+
+
 
 from escpos import printer
 import contextlib
@@ -27,7 +32,7 @@ from MorseImageCreator import MorseImageCreator
 WINDOW_WIDTH = 400
 WINDOW_HEIGHT = 400
 WINDOW_SURFACE = pygame.HWSURFACE | pygame.DOUBLEBUF | pygame.RESIZABLE
-
+USBLP_GET_STATUS = 0x060b
 DARK_BLUE = (3, 5, 54)
 
 # Pygame events
@@ -39,6 +44,10 @@ message_typing_timeout_event = pygame.USEREVENT + 5
 
 multi_line_config = Config("CENTER", 50, 20, 20, 10, 6, add_headers=True)
 single_line_config = Config("LEFT", 75, 20, 0, 20, 1, add_headers=False)
+
+
+def runningAsRoot() -> bool:
+    return os.getuid() == 0
 
 
 class PygameWrapper:
@@ -93,6 +102,50 @@ class PygameWrapper:
     @staticmethod
     def createPrinter() -> printer.Usb:
         return printer.Usb(0x28e9, 0x0289, out_ep=0x03, profile="ZJ-5870")
+
+    @staticmethod
+    def resetUsbPrinter():
+        subprocess.run(["sudo", "rmmod", "usblp"])
+        subprocess.run(["sudo", "modprobe", "usblp"])
+        subprocess.run(["sudo", "chmod", "666", "/dev/usb/lp1"])
+
+    @staticmethod
+    def hasPaper():
+        if not runningAsRoot():
+            # This check will only work if you are running as root.
+            logging.warning("Unable to check paper status, not running as root")
+            return
+        # Story time! The escpos library doesn't handle the paper checking correctly. For some reason the default
+        # usb printing driver does. But that driver doesn't play well if i want to send images. So, as any "sane"
+        # developer, it makes me think "Well, why not create a monster of frankenstein".
+        # But the problem with monsters are is that they start shouting "WHY HAVE YOU CREATED ME FATHER"
+        # That is where the resetUsbPrinter comes in.
+        # Every time we send any command over USB to the printer via escpos, the linux kernel is very graceful with
+        # removing the /dev/usb/lp1. The only way to get it back is, you guessed it, restart the module
+        # May god have mercy on my soul, for these sins should not give me any.
+        try:
+            PygameWrapper.resetUsbPrinter()
+            with open("/dev/usb/lp1", "r+b") as printer:
+                # Send paper status command
+                printer.write(b'\x10\x04\x04')
+                printer.flush()
+
+                # Get status using ioctl
+                status = struct.unpack('B', fcntl.ioctl(printer, USBLP_GET_STATUS, b'\x00'))[0]
+                result = f"{status:08b}"  # Convert status to binary
+                paper_present = result[2] == "1"  # Check paper bit
+
+                # Graceful close: Reset printer to avoid device lock
+                # printer.write(b'\x1b\x40')  # ESC @ (reset command)
+                # printer.flush()
+
+                return paper_present
+        except FileNotFoundError:
+            print("Printer not found at /dev/usb/lp1.")
+            return False
+        except Exception as e:
+            print("Error reading printer status:", e)
+            return False
 
     def _requestMessageToBePrinted(self) -> None:
         try:
